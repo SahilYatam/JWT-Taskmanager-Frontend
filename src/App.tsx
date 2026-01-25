53398bb1deaf81eddb3e8d3b6ca0b745a59064f7
Sahil Yatam <sahilyatam9@gmail.com>
2026-01-25 11:16:45 +0530

    chore: initialize repository and set up frontend

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="bg-blue-500">
+        <h1 className="text-center text-2xl text-white">Checking Typescript + Tailwindcss</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/api/axios.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const api = axios.create({
+    baseURL: "http://localhost:8000",
+    withCredentials: true,
+});
+
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("access");
+    if(token) config.headers.Authorization = `Bearer ${token}`;
+    return config
+})
